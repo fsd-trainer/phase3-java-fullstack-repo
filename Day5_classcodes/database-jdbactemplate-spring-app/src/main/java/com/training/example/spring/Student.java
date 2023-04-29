36d15a7327bd3d8f4cdd4ed11d834f70b7384c4d
priya <priyag0907@outlook.com>
2023-04-29 18:18:49 +0530

    say 5 classroom codes

---
+++ Day5_classcodes/database-jdbactemplate-spring-app/src/main/java/com/training/example/spring/Student.java
@@ -0,0 +1,47 @@
+package com.training.example.spring;
+
+public class Student {
+
+	private int id;
+	private String name;
+	private long mobile;
+	private String country;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public long getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(long mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", mobile=" + mobile + ", country=" + country + "]";
+	}
+
+}
